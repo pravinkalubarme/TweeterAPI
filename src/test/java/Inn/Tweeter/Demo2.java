4d6af41affa3338cf9c7092edae7383d68d1f869
pravin <pravinpkalubarme@gmail.com>
2020-04-17 16:35:12 +0530

    commit

---
+++ src/test/java/Inn/Tweeter/Demo2.java
@@ -0,0 +1,10 @@
+package Demo.Pattern;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		
+
+      String[] a = Payload.methodA();
+      System.out.println(Payload.methodA()[0]);
+    		
+}}
